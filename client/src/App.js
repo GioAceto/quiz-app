dda98f03e065c9e702b4544ac5cbebd2c2c07f35
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-09 20:12:54 -0400

    add state change onclick function to button

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './styles/styles.scss';
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import MainMenu from './components/MainMenu';
 import Quiz from './components/Quiz';
 import EndScreen from './components/EndScreen';
+
+import { QuizContext } from './Helpers/Contexts';
 
 function App() {
   const [gameState, setGameState] = useState("menu");
@@ -10,11 +12,11 @@ function App() {
   return (
     <div className="App">
       <h1>Quiz App</h1>
-
-      {gameState === "menu" && <MainMenu />}
-      {gameState === "quiz" && <Quiz />}
-      {gameState === "endScreen" && <EndScreen />}
-
+      <QuizContext.Provider value={{ gameState, setGameState }}>
+        {gameState === "menu" && <MainMenu />}
+        {gameState === "quiz" && <Quiz />}
+        {gameState === "endScreen" && <EndScreen />}
+      </QuizContext.Provider>
     </div>
   );
 }

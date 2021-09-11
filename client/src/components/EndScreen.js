1803ea63112e160b7ae63582310fb65d648a08bc
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-11 11:08:47 -0400

    add rendering for score in EndScreen component

--- client/src/components/EndScreen.js
+++ client/src/components/EndScreen.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { QuizContext } from '../helpers/Contexts';
+import { Questions } from "../helpers/QuestionBank";
 
 function EndScreen() {
+  const { score, setScore, setGameState } = useContext(QuizContext);
+
+  const restartQuiz = () => {
+    setScore = 0;
+    setGameState("menu");
+  }
+
   return (
     <div>
-
+      {""}
+      <h2>Quiz Finished</h2>
+      <h3> Score</h3>
+      <h3> {score} / {Questions.length} </h3>
+      <button onClick={restartQuiz}>Restart Quiz</button>
     </div>
   )
 }

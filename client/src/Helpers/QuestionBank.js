6d3c60eed4bdda0b674bf2dbe39b07395b7a7c14
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-11 09:23:08 -0400

    create QuestionBank file and Questions array

---
+++ client/src/Helpers/QuestionBank.js
@@ -0,0 +1,28 @@
+const Questions = [
+  {
+    prompt: "Which ocean is the largest by surface area?",
+    optionA: "Atlantic",
+    optionB: "Pacific",
+    optionC: "Indian",
+    optionD: "Arctic",
+    answer: "optionB"
+  },
+  {
+    prompt: "Which river is the longest?",
+    optionA: "Mississippi",
+    optionB: "Amazon",
+    optionC: "Mekong",
+    optionD: "Nile",
+    answer: "optionD"
+  },
+  {
+    prompt: "Which lake is the deepest?",
+    optionA: "Baikal",
+    optionB: "Vostok",
+    optionC: "Victoria",
+    optionD: "Crater",
+    answer: "optionA"
+  }
+]
+
+export default Questions;
